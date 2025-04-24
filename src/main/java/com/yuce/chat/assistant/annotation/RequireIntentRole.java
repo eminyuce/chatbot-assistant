72c85b2497dfcad38c1bef79a4c0ce0e366f2579
eminyuce <eminyuce@gmail.com>
2025-04-24 15:21:52 +0300

    Aspect role check is not working v2

--- src/main/java/com/yuce/chat/assistant/annotation/RequireIntentRole.java
+++ src/main/java/com/yuce/chat/assistant/annotation/RequireIntentRole.java
@@ -2,9 +2,8 @@ package com.yuce.chat.assistant.annotation;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
+@Retention(RetentionPolicy.RUNTIME) // <-- MUST be RUNTIME
+@Target(ElementType.METHOD)         // <-- MUST include METHOD
 public @interface RequireIntentRole {
     String[] value(); // list of roles required
 }
